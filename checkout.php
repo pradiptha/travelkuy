c7ce30edcb2f7ef878d1301915c81a0e76b2909c
nptayusherly <nptayusherly@gmail.com>
2019-11-04 17:32:41 +0800

    ini checkout keknya, tapi masih kurang rapi dikit

---
+++ checkout.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta name="description" content="Start your development with a Design System for Bootstrap 4.">
+  	<meta name="author" content="Creative Tim">
+  	<title>TravelKuy - Liburan Biar Santuy</title>
+  	<!-- Favicon -->
+  	<link href="assets/img/brand/logo-01.png" rel="icon" type="image/png">
+  	<!-- Fonts -->
+  	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  	<!-- Icons -->
+  	<link href="assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+  	<link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  	<!-- Argon CSS -->
+  	<link rel="stylesheet" type="text/css" href="checkout.css">
+  	<link type="text/css" href="assets/css/argon.css?v=1.1.0" rel="stylesheet">
+</head>
+<body style="background-color: #E1F5FF">
+	<?php include 'navbar.php' ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 bg-white mt-3 p-3">
+				<h1>Wisata Alam Bali</h1>
+				<p>Peserta&nbsp;&nbsp;: 2 orang</p>
+				<p>Lokasi Penjemputan&nbsp;: Jalan Imam Bonjol Denpasar</p>
+				<p>Keberangkatan&nbsp;: 20 November 2019</p>
+			</div>
+			<div class="col-sm-12 bg-white mt-3 p-3">
+				<h3 class="primary">Pembayaran</h3>
+				<p>Lakukan pembayaran ke No. Rek: xxxx-xxxx-xxxx</p>
+				<p>Sebesar Rp 5000000</p>
+				<p>Sampai 1 hari setelah checkout</p>
+			</div>
+			<div class="col-sm-12">
+				<br><br>
+				<div class="col-sm-6 float-left">
+					<button type="button" class="btn btn-primary btn-lg btn-block">EDIT PESANAN</button>
+				</div>
+				<div class="col-sm-6 float-left">
+					<button type="button" class="btn btn-danger btn-lg btn-block">CHECKOUT</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
