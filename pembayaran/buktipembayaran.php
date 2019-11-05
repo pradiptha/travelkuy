6d2d8a0fc7c5e6b4abf20302085771081264eaf5
nptayusherly <nptayusherly@gmail.com>
2019-11-05 15:21:37 +0800

    upload bukti pembarayan

--- pembayaran/buktipembayaran.php
+++ pembayaran/buktipembayaran.php
@@ -14,33 +14,106 @@
   	<link href="../assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
   	<link href="../assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
   	<!-- Argon CSS -->
-  	<link rel="stylesheet" type="text/css" href="">
+  	<link rel="stylesheet" type="text/css" href="bayar.css">
   	<link type="text/css" href="../assets/css/argon.css?v=1.1.0" rel="stylesheet">
+
+  	<!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 </head>
+
 <body style="background-color: #E1F5FF">
 	<?php include '../navbar/navbar.php' ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12 bg-white mt-3 p-3">
-				<h2 class="pl-3">UPLOAD BUKTI PEMBAYARAN</h2>
-				<table class="table table-responsive table-borderless">
-					<tr>
-						<td scope="col">Kode Pesanan</td>
-						<td scope="col">:</td>
-						<td scope="col">13081999</td>
-					</tr>
-				</table>
-				<div class="input-group p-3">
-				  	<div class="custom-file">
-				    	<input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
-				    	<label class="custom-file-label" for="inputGroupFile04">Choose file</label>
-				  	</div>
-				  	<div class="input-group-append">
-				    	<button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">UPLOAD</button>
-				  	</div>
-				</div>
+			<div class="col-sm-12 bg-white mt-3 p-3 mb-2 rounded">
+				<div class="panel panel-default">
+					<div class="panel-heading pl-3"><strong><h3>UPLOAD BUKTI PEMBAYARAN</h3></strong></div>
+					<table class="table table-responsive table-borderless">
+						<tr>
+							<td scope="col">Kode Pesanan</td>
+							<td scope="col">:</td>
+							<td scope="col">13081999</td>
+						</tr>
+					</table>
+					<div class="panel-body pl-3 pr-3">
+			          	<!-- Standar Form -->
+			          	<h5>Select files from your computer</h5>
+			          	<form action="" method="post" enctype="multipart/form-data" id="js-upload-form">
+			            	<div class="form-inline">
+			              		<div class="form-group mt-2 mb-2">
+			                		<input type="file" name="files[]" id="js-upload-files" multiple>
+			              		</div>
+			            	</div>
+			          	</form>
+
+			          	<!-- Drop Zone -->
+			          	<h5>Or drag and drop files below</h5>
+			          	<div class="upload-drop-zone" id="drop-zone">
+			            	Just drag and drop files here
+			          	</div>
+
+			          	<!-- Progress Bar -->
+			          	<div class="progress">
+			            	<div class="progress-bar text-center" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+			              		<span class="sr-only">60% Complete</span>
+			            	</div>
+			          	</div>
+
+			          	<!-- Upload Finished -->
+			          	<div class="js-upload-finished">
+			            	<h5>Processed files</h5>
+			            	<div class="list-group">
+			              		<a href="#" class="list-group-item list-group-item-success"><span class="badge alert-success pull-right">Success</span>image-01.jpg</a>
+			            	</div>
+			          	</div>			          	
+						<button type="button" class="btn btn-danger btn-lg mt-4">CANCEL</button>
+						<button type="button" class="btn btn-primary btn-lg mt-4">UPLOAD</button>
+			        </div>
+		    	</div>
 			</div>
 		</div>
 	</div>
 </body>
+<script type="text/javascript">
+	+ function($) {
+    'use strict';
+
+    // UPLOAD CLASS DEFINITION
+    // ======================
+
+    var dropZone = document.getElementById('drop-zone');
+    var uploadForm = document.getElementById('js-upload-form');
+
+    var startUpload = function(files) {
+        console.log(files)
+    }
+
+    uploadForm.addEventListener('submit', function(e) {
+        var uploadFiles = document.getElementById('js-upload-files').files;
+        e.preventDefault()
+
+        startUpload(uploadFiles)
+    })
+
+    dropZone.ondrop = function(e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function() {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function() {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
+
+	}(jQuery);
+</script>
 </html>
