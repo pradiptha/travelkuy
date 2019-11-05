ccf24d5602aefc7e6b6687d9edcd3a44bfd43de0
nptayusherly <nptayusherly@gmail.com>
2019-11-05 11:33:42 +0800

    ini login

---
+++ pembayaran/buktipembayaran.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta name="description" content="Start your development with a Design System for Bootstrap 4.">
+  	<meta name="author" content="Creative Tim">
+  	<title>TravelKuy - Liburan Biar Santuy</title>
+  	<!-- Favicon -->
+  	<link href="../assets/img/brand/fav.png" rel="icon" type="image/png">
+  	<!-- Fonts -->
+  	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  	<!-- Icons -->
+  	<link href="../assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+  	<link href="../assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  	<!-- Argon CSS -->
+  	<link rel="stylesheet" type="text/css" href="">
+  	<link type="text/css" href="../assets/css/argon.css?v=1.1.0" rel="stylesheet">
+</head>
+<body style="background-color: #E1F5FF">
+	<?php include '../navbar/navbar.php' ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 bg-white mt-3 p-3">
+				<h2 class="pl-3">UPLOAD BUKTI PEMBAYARAN</h2>
+				<table class="table table-responsive table-borderless">
+					<tr>
+						<td scope="col">Kode Pesanan</td>
+						<td scope="col">:</td>
+						<td scope="col">13081999</td>
+					</tr>
+				</table>
+				<div class="input-group p-3">
+				  	<div class="custom-file">
+				    	<input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
+				    	<label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+				  	</div>
+				  	<div class="input-group-append">
+				    	<button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">UPLOAD</button>
+				  	</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
