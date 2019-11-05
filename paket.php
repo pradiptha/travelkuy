1e1175ce6798493009476c4c66af7c2b2bc4b3e3
widiantariputri <ayutari16@gmail.com>
2019-11-05 10:44:22 +0800

    paket

---
+++ paket.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Start your development with a Design System for Bootstrap 4.">
+	<meta name="author" content="Creative Tim">
+	<title>TravelKuy - Liburan Biar Santuy</title>
+	<!-- Favicon -->
+	<link href="assets/img/brand/logo-01.png" rel="icon" type="image/png">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<!-- Icons -->
+	<link href="assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+	<link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Argon CSS -->
+	<link rel="stylesheet" type="text/css" href="css-paket.css">
+	<link type="text/css" href="assets/css/argon.css?v=1.1.0" rel="stylesheet">
+</head>
+<body style="background-color: #E1F5FF;">
+	<?php include 'navbar.php' ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 destinasi-uraian">
+				<img src="assets/img/icons/bali.jpg" class="img-thumbnail rounded">
+			</div>
+			<div class="col-sm-8 destinasi-uraian">
+				<div class="destinasi">
+					<h2>WISATA ALAM BALI</h2>
+				</div>
+				<div class="destinasi">
+					<p>Paket wisata alam yang memanjakan Anda mengelilingi bali, melihat bali, dari sudut pandang keindahan alamnya</p>
+				</div>
+				<div class="destinasi">
+					<h1 class="biaya-destinasi">Rp 2.500.000</h1>
+				</div>
+				<div class="tombol">
+				<button class="btn btn-1 btn-primary" type="button">Pesan Sekarang</button>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-xs-6 col-md-4">
+				<div class="tl">
+					<h3>TIMELINE</h3>
+					<hr class="hr1">	
+				</div>
+				<div class="garis">
+					
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-8 deskripsi-destinasi">
+				<div class="btn_desk">
+					<button class="btn btn-2 btn-primary" type="button">Deskripsi</button>
+				</div>
+				<div class="penjabaran">
+					<p>Paket wisata alam Bali terbagi menjadi beberapa destinasi dan tempat peristirahatan. Adapun fasilitas yang didapat, yaitu</p>
+					<ul>
+						<li>Destinasi wisata a</li>
+						<li>Destinasi wisata b</li>
+						<li>Destinasi wisata b</li>
+						<li>Penginapan hotel a</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
