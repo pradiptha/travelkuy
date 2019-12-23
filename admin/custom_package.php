f69c7369957ec9fe2da4af3fb364f77a54c1c4e7
widiantariputri <ayutari16@gmail.com>
2019-12-23 09:36:48 +0800

    tambahan admin

--- admin/custom_package.php
+++ admin/custom_package.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>custom package</title>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Slideshow</title>
-	<!-- Tell the browser to be responsive to screen width -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-	<!-- Ionicons -->
-	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-	<!-- DataTables -->
-	<link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-	<!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<!-- Tell the browser to be responsive to screen width -->
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<!-- Font Awesome -->
+  	<link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  	<!-- Ionicons -->
+  	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  	<!-- DataTables -->
+  	<link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+  	<!-- Theme style -->
+  	<link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  	<!-- Google Font: Source Sans Pro -->
+  	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini">
 	<div class="wrapper">
@@ -33,11 +33,11 @@
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-left">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Slideshow</li>
+                <li class="breadcrumb-item active">Data Custom Package</li>
               </ol>
             </div>
             <div class="col-sm-6 ">
-              <button type="button" class="btn btn-primary float-sm-right btn-sm" data-toggle="modal" data-target="#modaltambah">Tambah</button>
+              <button type="button" class="btn btn-outline-primary d-inline-block float-sm-right btn-sm" data-toggle="modal" data-target="#exampleModal">Tambah</button>
             </div>
           </div>
         </div><!-- /.container-fluid -->
@@ -51,41 +51,31 @@
                 <table id="example2" class="table table-bordered table-hover">
                   <thead>
                   <tr>
-                    <th style="width: 40%">URL Foto</th>
-                    <th style="width: 40%">Link</th>
-                    <th style="width: 5%" class="text-center">Status</th>
-                    <th style="width: 15%">Action</th>
+                    <th>ID Paket</th>
+                    <th>Nama Paket</th>
+                    <th>Deskripsi</th>
+                    <th>Total Biaya</th>
+                    <th>Action</th>
                   </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td>D01</td>
-                      <td>vdger.php</td>
-                      <td class="project-state">
-                        <div class="dropdown">
-                          <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Status
-                          </button>
-                          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                            <button class="btn btn-success dropdown-item" type="button">Active</button>
-                            <button class="btn btn-danger dropdown-item" type="button">Inactive</button>
-                          </div>
-                        </div>
-                      </td>
+                      <td>Destinasi Bali</td>
+                      <td>Terletak di Pulau Bali dengan jml destinasi 9 lokasi</td>
+                      <td> 4.000.000</td>
                       <td>
                         <div>
-                          <div>
-                            <a class="btn btn-info btn-sm" href="#">
-                                <i class="fas fa-pencil-alt">
-                                </i>
-                                Edit
-                            </a>
-                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modaldelete">
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Edit
+                          </a>
+                          <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modaldelete">
                               <i class="fas fa-trash">
                                 </i>
                                 Delete
-                            </button>
-                          </div>
+                          </button>
                         </div>
                       </td>
                     </tr>
@@ -102,31 +92,38 @@
       </section>
       <!-- /.content -->
     </div>
-    <!-- modal tambah -->
-    <div class="modal fade" id="modaltambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Slideshow</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Custom Package</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
             <form action="index.html" method="post">
-              <div class="input-group mb-6">
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                    </div>
-                </div>
-              </div>
-              <div class="input-group mb-6">
-                <input type="text" class="form-control" placeholder="Link">
+              <div class="input-group mb-4">
+                <input type="text" class="form-control" placeholder="Package name">
                 <div class="input-group-append">
                   <div class="input-group-text">
-                    <span class="fas fa-link"></i></span>
+                    <span class="fas fa-user"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="input-group mb-4">
+                <textarea name="comment" form="usrform" placeholder="Description" style=" width:425px; padding: 10px;"></textarea>
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="input-group mb-4">
+                <input type="text" class="form-control" placeholder="Price">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-dollar-sign"></span>
                   </div>
                 </div>
               </div>
@@ -139,7 +136,6 @@
         </div>
       </div>
     </div>
-    <!-- modal tambah -->
     <!-- modal delete -->
     <div class="modal fade" id="modaldelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
@@ -149,7 +145,7 @@
               <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
           </div>
           <div class="modal-body">
-            <p>Do you really want to delete these picture?</p>
+            <p>Do you really want to delete these package?</p>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-info" data-dismiss="modal">Cancel</button>
--- admin/destination.php
+++ admin/destination.php
@@ -1,25 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Default Package</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<title>Destination</title>
+	meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<!-- Tell the browser to be responsive to screen width -->
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<!-- Font Awesome -->
+  	<link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  	<!-- Ionicons -->
+  	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  	<!-- DataTables -->
+  	<link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+  	<!-- Theme style -->
+  	<link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  	<!-- Google Font: Source Sans Pro -->
+  	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini">
-  <div class="wrapper">
+	<div class="wrapper">
     <!-- Navbar -->
     <?php include 'navbar.php' ?>
     <!-- /.navbar -->
@@ -34,7 +33,7 @@
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-left">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Data Default Package</li>
+                <li class="breadcrumb-item active">Data Destination</li>
               </ol>
             </div>
             <div class="col-sm-6 ">
@@ -67,8 +66,16 @@
                       <td> 4.000.000</td>
                       <td>
                         <div>
-                          <a href="#"><i class="far fa-edit"></i></a>
-                          <a href="#"><i class="far fa-trash-alt"></i></a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Edit
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Delete
+                          </a>
                         </div>
                       </td>
                     </tr>
@@ -89,7 +96,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Default Package</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Custom Package</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -105,7 +112,7 @@
                 </div>
               </div>
               <div class="input-group mb-4">
-                <textarea name="comment" form="usrform" placeholder="description" style=" width:425px; padding: 10px;"></textarea>
+                <textarea name="comment" form="usrform" placeholder="Description" style=" width:425px; padding: 10px;"></textarea>
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-envelope"></span>
@@ -116,7 +123,7 @@
                 <input type="text" class="form-control" placeholder="Price">
                 <div class="input-group-append">
                   <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
+                    <span class="fas fa-dollar-sign"></span>
                   </div>
                 </div>
               </div>
@@ -134,12 +141,5 @@
       <!-- Control sidebar content goes here -->
     </aside>
     <!-- /.control-sidebar -->
-  </div>
-  <script>
-    var autoExpand = function (field) {
-    // Reset field height
-      field.style.height = 'inherit';
-    };
-  </script>
 </body>
 </html>
